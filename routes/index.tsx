a5683fe81b0f663f8fe12fe64700273185b9a93b
Jonas <jonas@jmp.blue>
2024-09-22 19:50:03 +0200

    init 2

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,25 @@
-import { Head } from "$fresh/runtime.ts";
+import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>Fresh App</title>
-			</Head>
-			<div class="p-4 mx-auto max-w-screen-md">
-				<img
-					src="/logo.svg"
-					class="w-32 h-32"
-					alt="the fresh logo: a sliced lemon dripping with juice"
-				/>
-				<p class="my-6">
-					Welcome to `fresh`. Try updating this message in the
-					./routes/index.tsx file, and refresh.
-				</p>
-				<Counter start={3} />
-			</div>
-		</>
-	);
+  const count = useSignal(3);
+  return (
+    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        <img
+          class="my-6"
+          src="/logo.svg"
+          width="128"
+          height="128"
+          alt="the Fresh logo: a sliced lemon dripping with juice"
+        />
+        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <p class="my-4">
+          Try updating this message in the
+          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+        </p>
+        <Counter count={count} />
+      </div>
+    </div>
+  );
 }
